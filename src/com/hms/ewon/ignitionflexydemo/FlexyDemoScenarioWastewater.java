d2eda1983d81c4636473e35dc21298f9b1f22536
Alex Hawk (HMS) <alha@hms.se>
2019-07-08 14:26:15 -0400

    Remove Temp Wastewater Treatment Scenario Code
    Remove code formerly used for testing from the wastewater treatment scenario

--- src/com/hms/ewon/ignitionflexydemo/FlexyDemoScenarioWastewater.java
+++ src/com/hms/ewon/ignitionflexydemo/FlexyDemoScenarioWastewater.java
@@ -1,8 +1,5 @@
 package com.hms.ewon.ignitionflexydemo;
 
-import com.hms.ewon.ignitionflexydemo.devices.FlexyDemoPump;
-import com.hms.ewon.ignitionflexydemo.devices.FlexyDemoTank;
-
 import java.io.InputStream;
 
 /**
@@ -25,31 +22,6 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
      * Perform initial generation and setup of this <code>FlexyDemoScenario</code>
      */
     protected void genScenario() {
-        int pumpPressureLowPSI = 100;
-        int pumpPressureHighPSI = 200;
-        int pumpPressureIdealPSI = 150;
-        int pumpPowerLowVolt = 115;
-        int pumpPowerHighVolt = 122;
-        int pumpPowerIdealVolt = 120;
-        FlexyDemoPump pumpOne = new FlexyDemoPump( "IntakePump", pumpPressureLowPSI, pumpPressureHighPSI, pumpPressureIdealPSI,
-                pumpPowerLowVolt, pumpPowerHighVolt, pumpPowerIdealVolt );
-
-        int tankInPressureLowPSI = 30;
-        int tankInPressureHighPSI = 100;
-        int tankInPressureIdealPSI = 55;
-        int tankOutPressureLowPSI = 20;
-        int tankOutPressureHighPSI = 90;
-        int tankOutPressureIdealPSI = 45;
-        int tankFillLow = 0;
-        int tankFillHigh = 10;
-        int tankFillIdeal = 85;
-        int tankProcessTimeSecs = 7;
-        FlexyDemoTank settleTank = new FlexyDemoTank( "SettleTank01",  tankInPressureLowPSI,tankInPressureHighPSI,
-                tankInPressureIdealPSI,tankOutPressureLowPSI,tankOutPressureHighPSI,tankOutPressureIdealPSI,tankFillLow,
-                tankFillHigh,tankFillIdeal,tankProcessTimeSecs );
-        assert devices != null;
-        devices.add( pumpOne );
-        devices.add( settleTank );
     }
 
     /**
