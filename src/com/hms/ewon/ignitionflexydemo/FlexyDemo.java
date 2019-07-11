c86fa341b014656669f99209d91b8d763123a0b2
Alex Hawk (HMS) <alha@hms.se>
2019-07-11 10:22:49 -0400

    Update Abstract Demo Classes to New Tag System
    Update the abstract and base demo classes accordingly to allow for use of the new tag system.

--- src/com/hms/ewon/ignitionflexydemo/FlexyDemo.java
+++ src/com/hms/ewon/ignitionflexydemo/FlexyDemo.java
@@ -2,14 +2,8 @@ package com.hms.ewon.ignitionflexydemo;
 
 import com.ewon.ewonitf.EWException;
 import com.ewon.ewonitf.TagControl;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Random;
 
 /**
@@ -19,23 +13,10 @@ import java.util.Random;
  */
 public class FlexyDemo
 {
-
-   private static int TAG_ID_COUNTER = 1;
-
    /**
     * Number of Scenarios Available
     */
    private static final int NUM_SCENARIOS = 2;
-
-   /**
-    * Constant for Tag Floating Point Type
-    */
-   public static final int TAG_FLOAT = 1;
-
-   /**
-    * Constant for Tag Integer Type
-    */
-   public static final int TAG_INT = 2;
 
    /**
     * Constant for length of each Demo cycle
@@ -72,16 +53,6 @@ public class FlexyDemo
     */
    private static ArrayList flexyDemoScenarios = new ArrayList();
 
-   /**
-    * eWON Tag To Stop Demo (0=RUN,1=STOP)
-    */
-   private static TagControl demoStopTag;
-
-   /**
-    * eWON Tag to Select Scenario (0=NONE)
-    */
-   private static TagControl demoScenarioTag;
-
 
    /**
     * Add a <code>FlexyDemoScenario</code> to this <code>FlexyDemo</code>
@@ -98,32 +69,14 @@ public class FlexyDemo
     *
     * @return Index of chosen scenario
     */
-   private static int getChosenScenario()
+   private static int getChosenScenario() throws EWException
    {
-      int chosen = ( int ) demoScenarioTag.getTagValueAsLong() - 1;
+      int chosen = ( int ) FlexyDemoTagManager.getTagAsInt( "DEMOSCENARIO" ) - 1;
       if ( chosen >= NUM_SCENARIOS ) {
          System.out.println( "FlexyDemo couldn't locate the selected scenario. Reverting to no scenario." );
          return -1;
       }
       return chosen;
-   }
-
-   /**
-    * Reset <code>FlexyDemo</code> system tags to their default value. Specifically, DEMOSTOP and DEMOSCENARIO are reset
-    * to 0.
-    */
-   private static void resetAppTags()
-   {
-      try {
-         demoStopTag.setTagValueAsInt( DEMO_NOT_STOPPED );
-         // Add 1 necessary to account for internal 0-based vs Flexy 1-based
-         demoScenarioTag.setTagValueAsInt( NO_SCENARIO_SELECTED + 1 );
-      }
-      catch ( EWException e ) {
-         System.out.println( "FlexyDemo encountered an error while resetting application tags. Application will " +
-                             "now terminate to prevent undefined behavior. ID: FD01" );
-         System.exit( -1 );
-      }
    }
 
    /**
@@ -274,9 +227,9 @@ public class FlexyDemo
     *
     * @return true if this <code>FlexyDemo</code> is active and running.
     */
-   public static boolean isRunning()
+   public static boolean isRunning() throws EWException
    {
-      return demoStopTag.getTagValueAsLong() < DEMO_STOPPED;
+      return FlexyDemoTagManager.getTagAsInt( "DEMOSTOP" ) < DEMO_STOPPED;
    }
 
    /**
@@ -284,9 +237,17 @@ public class FlexyDemo
     */
    private static void runDemo()
    {
-      while ( isRunning() ) {
-         // Notice: Subtract 1 necessary as Java ArrayList is 0-based.
-         int chosenScenario = getChosenScenario();
+      boolean isRunning = true;
+      while ( isRunning ) {
+         // UPDATE SELECTED SCENARIO
+         int chosenScenario = currentScenario;
+         try {
+            chosenScenario = getChosenScenario();
+         }
+         catch ( EWException e ) {
+            System.out.println( "[FlexyDemo] An error occurred while updating the current running demo scenario. Demo" +
+                                " scenario switching may be unavailable.");
+         }
 
          // Stop Previous Scenario
          if ( chosenScenario != currentScenario ) {
@@ -300,9 +261,19 @@ public class FlexyDemo
             // Start New Selected Scenario
             if ( chosenScenario >= 0 && chosenScenario < flexyDemoScenarios.size() ) {
                FlexyDemoScenario nextRunning = ( FlexyDemoScenario ) flexyDemoScenarios.get( chosenScenario );
-               copyVarLst();
                nextRunning.start();
             }
+         }
+
+         // UPDATE isRunning TAG
+         try
+         {
+            isRunning = isRunning();
+         }
+         catch ( EWException e ) {
+            System.out.println( "[FlexyDemo] An error occurred while checking demo run status. Application will now " +
+                                "terminate to prevent unexpected behavior.");
+            System.exit( -1 );
          }
 
          // WAIT ONE SECOND BEFORE LOOPING AGAIN
@@ -317,99 +288,12 @@ public class FlexyDemo
       }
    }
 
-   public static void createAndInitTags()
+   private static void uploadTagConfig()
    {
-      try {
-         demoStopTag = new TagControl( "DEMOSTOP" );
-         demoScenarioTag = new TagControl( "DEMOSCENARIO" );
-      }
-      catch ( Exception e ) {
-         try {
-            System.out.println(
-                  "FlexyDemo was unable to locate required tags on your system. They will now be " + "created." );
-            FlexyDemo.copyVarLst();
-            demoStopTag = new TagControl( "DEMOSTOP" );
-            demoScenarioTag = new TagControl( "DEMOSCENARIO" );
-         }
-         catch ( Exception ex ) {
-            ex.printStackTrace();
-            System.out.println( "FlexyDemo was still unable to locate required tags on your system. Application " +
-                                "will now terminate to prevent undefined behavior. ID: FD03" );
-            System.exit( -1 );
-         }
-      }
-   }
+      if (flexyDemoScenarios.size() == 0) System.out.println( "[FlexyDemo] No scenarios configured during Tag Config " +
+                                                              "Upload");
 
-   /**
-    * Create and return string to be used for tag entry in Var_Lst.csv
-    *
-    * @param name Name of Tag
-    * @param type Type of Tag (2 if Int, 1 if Floating Point)
-    *
-    * @return var_lst ready string for tag
-    */
-   static String createVarLstTagString( String name, int type )
-   {
-      Date date = new Date();
-      SimpleDateFormat dateFormat = new SimpleDateFormat( "D T" );
-      final String builtRes = FlexyDemo.TAG_ID_COUNTER + ";\"" + name + "\";\"\";\"MEM\";\"\";\"" + name +
-                              "\";1.000000;0.000000;0;0;0;1;0;0;0;0;0;0;1;0;\"\";0.000000;0.000000;0;0.000000;0;0;0;0;" +
-                              "1;600;10;-1.000000;0;;;1;1.000000;0.000000;;\"\";\"\";\"\";\"\";;;\"\";\"\";;\"\";;\"\";" +
-                              "\"\";0;" + type + ";0;\"" + dateFormat.format( date ) + "\";0;65472;0" + "\r\n";
-      FlexyDemo.TAG_ID_COUNTER++;
-      return builtRes;
-   }
-
-   private static InputStream createLiveVarLst()
-   {
-      // Add Header of Var_Lst.csv to Built String
-      String builtVarLst =
-            "\"Id\";\"Name\";\"Description\";\"ServerName\";\"TopicName\";\"Address\";\"Coef\";\"Offset\";\"LogEnabled\";" +
-            "\"AlEnabled\";\"AlBool\";\"MemTag\";\"MbsTcpEnabled\";\"MbsTcpFloat\";\"SnmpEnabled\";\"RTLogEnabled\";" +
-            "\"AlAutoAck\";\"ForceRO\";\"SnmpOID\";\"AutoType\";\"AlHint\";\"AlHigh\";\"AlLow\";\"AlTimeDB\";" +
-            "\"AlLevelDB\";\"IVGroupA\";\"IVGroupB\";\"IVGroupC\";\"IVGroupD\";\"PageId\";\"RTLogWindow\";\"RTLogTimer\";" +
-            "\"LogDB\";\"LogTimer\";\"AlLoLo\";\"AlHiHi\";\"MbsTcpRegister\";\"MbsTcpCoef\";\"MbsTcpOffset\";\"EEN\";" +
-            "\"ETO\";\"ECC\";\"ESU\";\"EAT\";\"ESH\";\"SEN\";\"STO\";\"SSU\";\"TEN\";\"TSU\";\"FEN\";\"FFN\";\"FCO\";" +
-            "\"KPI\";\"Type\";\"AlStat\";\"ChangeTime\";\"TagValue\";\"TagQuality\";\"AlType\"\r\n";
-
-      // Add Basic Tags for Demo
-      builtVarLst += createVarLstTagString( "DEMOSTOP", TAG_INT );
-      builtVarLst += createVarLstTagString( "DEMOSCENARIO", TAG_INT );
-
-      // Check if a Scenario is Active
-      if ( currentScenario > -1 ) {
-         FlexyDemoScenario running = ( FlexyDemoScenario ) flexyDemoScenarios.get( currentScenario );
-         builtVarLst += running.genVarLst();
-      }
-      return new ByteArrayInputStream( builtVarLst.getBytes() );
-   }
-
-   /**
-    * Handle Copying Source File to Destination File
-    */
-   static void copyVarLst()
-   {
-      try {
-         System.out.println( "FlexyDemo is starting FTP Tag Configuration -- Please Wait!" );
-         FTPClient ftpClient = new FTPClient();
-         ftpClient.connect( "localhost" );
-         if ( !FTPReply.isPositiveCompletion( ftpClient.getReplyCode() ) ) {
-            ftpClient.disconnect();
-            System.out.println( "FlexyDemo encountered an error while preparing FTP Tag Configuration. " +
-                                "Application will now terminate to prevent undefined behavior. ID: FD04" );
-         }
-         ftpClient.login( "adm", "adm" );
-         ftpClient.setFileType( FTP.BINARY_FILE_TYPE );
-         ftpClient.storeFile( "var_lst.csv", createLiveVarLst() );
-         ftpClient.logout();
-         ftpClient.disconnect();
-         System.out.println( "FlexyDemo has finished FTP Tag Configuration -- Success!" );
-      }
-      catch ( IOException e ) {
-         e.printStackTrace();
-         System.out.println( "FlexyDemo encountered an error while configuring tags via FTP. Application will now " +
-                             "terminate to prevent undefined behavior. ID: FD05" );
-      }
+      FlexyDemoTagManager.uploadTagConfig( flexyDemoScenarios );
    }
 
 
@@ -426,15 +310,12 @@ public class FlexyDemo
       System.out
             .println( "FlexyDemo has started! Tags and Data are simulated and may not reflect actual system data." );
 
-      // Create TagControl Objects and Ensure Tags Exists on Flexy
-      createAndInitTags();
-
-      // RESET TAGS
-      resetAppTags();
-
       // CREATE DEMO
       addDemoScenario( new FlexyDemoScenarioWastewater( "ExampleWastewaterPlant" ) );
       addDemoScenario( new FlexyDemoScenarioPackageSorting( "ExamplePackageSortFacility" ) );
+
+      // PERFORM AUTOMATIC TAG CONFIGURATION
+      uploadTagConfig();
 
       // RUN DEMO
       runDemo();
