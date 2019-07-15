aa01e9b083fd0499b3ba19433c12a8eafe43362e
Alex Hawk (HMS) <alha@hms.se>
2019-07-15 09:35:19 -0400

    Remove excess devices

--- src/com/hms/ewon/ignitionflexydemo/FlexyDemoScenarioWastewater.java
+++ src/com/hms/ewon/ignitionflexydemo/FlexyDemoScenarioWastewater.java
@@ -24,8 +24,7 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
    */
   protected void genScenario() {
     // Lift Stations
-    String[] liftStation_names = {"LiftStation-A01", "LiftStation-A02", "LiftStation-B01",
-        "LiftStation-B02"};
+    String[] liftStation_names = {"LiftStation-A01", "LiftStation-B01"};
     int liftStation_flowLowGPM = 8000;
     int liftStation_flowHighGPM = 10000;
     int liftStation_flowIdealGPM = 9000;
@@ -36,10 +35,7 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Bar Screens
-    String[] barScreen_names =
-        {"BarScreen-A01", "BarScreen-A02", "BarScreen-A03", "BarScreen-A04", "BarScreen-B01",
-            "BarScreen-B02",
-            "BarScreen-B03", "BarScreen-B04"};
+    String[] barScreen_names = {"BarScreen-A01", "BarScreen-A02", "BarScreen-B01", "BarScreen-B02"};
     int barScreen_flowLowGPM = 4000;
     int barScreen_flowHighGPM = 5000;
     int barScreen_flowIdealGPM = 4500;
@@ -50,10 +46,8 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Grit Remover
-    String[] gritRemover_names =
-        {"GritRemover-A01", "GritRemover-A02", "GritRemover-A03", "GritRemover-A04",
-            "GritRemover-B01",
-            "GritRemover-B02", "GritRemover-B03", "GritRemover-B04"};
+    String[] gritRemover_names = {"GritRemover-A01", "GritRemover-A02", "GritRemover-B01",
+        "GritRemover-B02"};
     int gritRemover_flowLowGPM = 3800;
     int gritRemover_flowHighGPM = 4800;
     int gritRemover_flowIdealGPM = 4500;
@@ -69,16 +63,10 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Pumps for Primary Settle Tank
-    String[] pumpSettlePrimary_names =
-        {"PumpSettlePrimary-A01", "PumpSettlePrimary-A02", "PumpSettlePrimary-A03",
-            "PumpSettlePrimary-A04",
-            "PumpSettlePrimary-B01", "PumpSettlePrimary-B02", "PumpSettlePrimary-B03",
-            "PumpSettlePrimary-B04"};
-    boolean[] pumpSettlePrimary_powerStatus =
-        {FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_OFF};
+    String[] pumpSettlePrimary_names = {"PumpSettlePrimary-A01", "PumpSettlePrimary-A02",
+        "PumpSettlePrimary-B01", "PumpSettlePrimary-B02"};
+    boolean[] pumpSettlePrimary_powerStatus = {FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_OFF,
+        FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_ON};
     int pumpSettlePrimary_flowLowGPM = 3800;
     int pumpSettlePrimary_flowHighGPM = 4800;
     int pumpSettlePrimary_flowIdealGPM = 4500;
@@ -89,15 +77,12 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Primary Settle Tanks
-    String[] settleTankPrimary_names =
-        {"SettleTankPrimary-A01", "SettleTankPrimary-A02", "SettleTankPrimary-A03",
-            "SettleTankPrimary-A04",
-            "SettleTankPrimary-B01", "SettleTankPrimary-B02", "SettleTankPrimary-B03",
-            "SettleTankPrimary-B04"};
-    int[] settleTankPrimary_fillLowPerc = {30, 60, 45, 70, 97, 10, 30, 95};
-    int[] settleTankPrimary_fillIdealPerc = {35, 62, 50, 75, 97, 15, 35, 95};
-    int[] settleTankPrimary_fillHighPerc = {40, 64, 55, 80, 98, 20, 40, 96};
-    int[] settleTankPrimary_drainOpenPerc = {0, 41, 0, 0, 0, 0, 0, 0};
+    String[] settleTankPrimary_names = {"SettleTankPrimary-A01", "SettleTankPrimary-A02",
+        "SettleTankPrimary-B01", "SettleTankPrimary-B02"};
+    int[] settleTankPrimary_fillLowPerc = {30, 60, 97, 10};
+    int[] settleTankPrimary_fillIdealPerc = {35, 62, 97, 15};
+    int[] settleTankPrimary_fillHighPerc = {40, 64, 98, 20};
+    int[] settleTankPrimary_drainOpenPerc = {0, 41, 0, 0};
     for (int i = 0; i < settleTankPrimary_names.length; i++) {
       devices.add(new FlexyDemoSettlingDrainTank(settleTankPrimary_names[i],
           settleTankPrimary_fillLowPerc[i],
@@ -174,25 +159,21 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
 
     // Aeration Tanks
     String[] aerationTank_names =
-        {"AerationTank-A01", "AerationTank-A02", "AerationTank-A03", "AerationTank-A04",
-            "AerationTank-B01",
-            "AerationTank-B02", "AerationTank-B03", "AerationTank-B04"};
-    int[] aerationTank_fillLowPerc = {91, 40, 90, 23, 92, 50, 90, 90};
-    int[] aerationTank_fillIdealPerc = {91, 42, 90, 24, 92, 51, 90, 90};
-    int[] aerationTank_fillHighPerc = {92, 44, 91, 25, 93, 52, 91, 91};
-    int[] aerationTank_oxygenLowPPM = {40, 8, 60, 10, 18, 10, 50, 50};
-    int[] aerationTank_oxygenIdealPPM = {45, 9, 62, 11, 19, 11, 52, 52};
-    int[] aerationTank_oxygenHighPPM = {50, 10, 64, 12, 20, 12, 54, 54};
-    int[] aerationTank_bodLowPPM = {50, 100, 70, 0, 20, 0, 65, 65};
-    int[] aerationTank_bodIdealPPM = {51, 110, 75, 1, 25, 1, 70, 70};
-    int[] aerationTank_bodHighPPM = {52, 120, 80, 2, 30, 2, 75, 75};
-    int[] aerationTank_pressureLowPSI = {12, 1, 12, 1, 13, 0, 12, 12};
-    int[] aerationTank_pressureIdealPSI = {13, 2, 13, 2, 14, 1, 13, 13};
-    int[] aerationTank_pressureHighPSI = {14, 3, 14, 3, 15, 2, 14, 14};
-    boolean[] aerationTank_powerStatus =
-        {FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_OFF};
+        {"AerationTank-A01", "AerationTank-A02", "AerationTank-B01", "AerationTank-B02"};
+    int[] aerationTank_fillLowPerc = {91, 40, 92, 50};
+    int[] aerationTank_fillIdealPerc = {91, 42, 92, 51};
+    int[] aerationTank_fillHighPerc = {92, 44, 93, 52};
+    int[] aerationTank_oxygenLowPPM = {40, 8, 18, 10};
+    int[] aerationTank_oxygenIdealPPM = {45, 9, 19, 11};
+    int[] aerationTank_oxygenHighPPM = {50, 10, 20, 12};
+    int[] aerationTank_bodLowPPM = {50, 100, 20, 0};
+    int[] aerationTank_bodIdealPPM = {51, 110, 25, 1};
+    int[] aerationTank_bodHighPPM = {52, 120, 30, 2};
+    int[] aerationTank_pressureLowPSI = {12, 1, 13, 0};
+    int[] aerationTank_pressureIdealPSI = {13, 2, 14, 1};
+    int[] aerationTank_pressureHighPSI = {14, 3, 15, 2};
+    boolean[] aerationTank_powerStatus = {FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON,
+        FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_ON};
     for (int i = 0; i < aerationTank_names.length; i++) {
       devices.add(new FlexyDemoAerationTank(aerationTank_names[i], aerationTank_pressureLowPSI[i],
           aerationTank_pressureHighPSI[i], aerationTank_pressureIdealPSI[i],
@@ -205,17 +186,13 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
 
     // Pumps for Secondary Settle Tank
     String[] pumpSettleSecondary_names =
-        {"PumpSettleSecondary-A01", "PumpSettleSecondary-A02", "PumpSettleSecondary-A03",
-            "PumpSettleSecondary-A04", "PumpSettleSecondary-B01", "PumpSettleSecondary-B02",
-            "PumpSettleSecondary-B03", "PumpSettleSecondary-B04"};
-    boolean[] pumpSettleSecondary_powerStatus =
-        {FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_OFF,
-            FlexyDemoFlexy.PWR_ON,
-            FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_ON, FlexyDemoFlexy.PWR_OFF,
-            FlexyDemoFlexy.PWR_OFF};
-    int[] pumpSettleSecondary_flowLowGPM = {3000, 3000, 3000, 3000, 3500, 3500, 3500, 3500};
-    int[] pumpSettleSecondary_flowHighGPM = {4250, 4250, 4250, 4250, 4250, 4250, 4250, 4250};
-    int[] pumpSettleSecondary_flowIdealGPM = {3500, 3500, 3500, 3500, 4000, 4000, 4000, 4000};
+        {"PumpSettleSecondary-A01", "PumpSettleSecondary-A02", "PumpSettleSecondary-B01",
+            "PumpSettleSecondary-B02"};
+    boolean[] pumpSettleSecondary_powerStatus = {FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_OFF,
+        FlexyDemoFlexy.PWR_OFF, FlexyDemoFlexy.PWR_ON};
+    int[] pumpSettleSecondary_flowLowGPM = {3000, 3000, 3500, 3500};
+    int[] pumpSettleSecondary_flowHighGPM = {4250, 4250, 4250, 4250};
+    int[] pumpSettleSecondary_flowIdealGPM = {3500, 3500, 4000, 4000};
     for (int i = 0; i < pumpSettleSecondary_names.length; i++) {
       devices.add(new FlexyDemoPump(pumpSettleSecondary_names[i], pumpSettleSecondary_flowLowGPM[i],
           pumpSettleSecondary_flowHighGPM[i], pumpSettleSecondary_flowIdealGPM[i],
@@ -223,13 +200,11 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Secondary Settle Tanks
-    String[] settleTankSecondary_names =
-        {"SettleTankSecondary-A01", "SettleTankSecondary-A02", "SettleTankSecondary-A03",
-            "SettleTankSecondary-A04", "SettleTankSecondary-B01", "SettleTankSecondary-B02",
-            "SettleTankSecondary-B03", "SettleTankSecondary-B04"};
-    int[] settleTankSecondary_fillLowPerc = {96, 91, 94, 75, 93, 46, 90, 32};
-    int[] settleTankSecondary_fillIdealPerc = {96, 91, 94, 76, 93, 47, 90, 33};
-    int[] settleTankSecondary_fillHighPerc = {97, 92, 95, 77, 94, 48, 91, 34};
+    String[] settleTankSecondary_names = {"SettleTankSecondary-A01", "SettleTankSecondary-A02",
+        "SettleTankSecondary-B01", "SettleTankSecondary-B02"};
+    int[] settleTankSecondary_fillLowPerc = {96, 91, 73, 46};
+    int[] settleTankSecondary_fillIdealPerc = {96, 91, 74, 47};
+    int[] settleTankSecondary_fillHighPerc = {97, 92, 75, 48};
     for (int i = 0; i < settleTankSecondary_names.length; i++) {
       devices.add(new FlexyDemoSettlingTank(settleTankSecondary_names[i],
           settleTankSecondary_fillLowPerc[i],
@@ -238,15 +213,12 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Chlorinator Valve
-    String[] chlorinatorValve_names =
-        {"ValveChlorinator-A01", "ValveChlorinator-A02", "ValveChlorinator-A03",
-            "ValveChlorinator-A04",
-            "ValveChlorinator-B01", "ValveChlorinator-B02", "ValveChlorinator-B03",
-            "ValveChlorinator-B04"};
-    boolean[] chlorinatorValve_open = {false, false, false, false, false, false, false, true};
-    int[] chlorinatorValve_flowLowGPM = {1250, 1250, 1250, 1250, 1250, 1250, 1250, 1250};
-    int[] chlorinatorValve_flowIdealGPM = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
-    int[] chlorinatorValve_flowHighGPM = {1750, 1750, 1750, 1750, 1750, 1750, 1750, 1750};
+    String[] chlorinatorValve_names = {"ValveChlorinator-A01", "ValveChlorinator-A02",
+        "ValveChlorinator-B01", "ValveChlorinator-B02"};
+    boolean[] chlorinatorValve_open = {false, false, true, false};
+    int[] chlorinatorValve_flowLowGPM = {1250, 1250, 1250, 1250};
+    int[] chlorinatorValve_flowIdealGPM = {1500, 1500, 1500, 1500};
+    int[] chlorinatorValve_flowHighGPM = {1750, 1750, 1750, 1750};
     for (int i = 0; i < chlorinatorValve_names.length; i++) {
       devices.add(new FlexyDemoValve(chlorinatorValve_names[i], chlorinatorValve_open[i],
           chlorinatorValve_flowLowGPM[i], chlorinatorValve_flowHighGPM[i],
@@ -254,14 +226,12 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // Chlorinators
-    String[] chlorinator_names =
-        {"Chlorinator-A01", "Chlorinator-A02", "Chlorinator-A03", "Chlorinator-A04",
-            "Chlorinator-B01",
-            "Chlorinator-B02", "Chlorinator-B03", "Chlorinator-B04"};
-    boolean[] chlorinator_egressValveOpen = {true, false, false, false, false, false, false, false};
-    int[] chlorinator_fillLowPerc = {53, 96, 98, 98, 95, 94, 94, 65};
-    int[] chlorinator_fillIdealPerc = {54, 96, 98, 98, 95, 94, 94, 66};
-    int[] chlorinator_fillHighPerc = {55, 97, 99, 99, 96, 95, 95, 67};
+    String[] chlorinator_names = {"Chlorinator-A01", "Chlorinator-A02", "Chlorinator-B01",
+        "Chlorinator-B02"};
+    boolean[] chlorinator_egressValveOpen = {true, false, false, false};
+    int[] chlorinator_fillLowPerc = {53, 96, 95, 94};
+    int[] chlorinator_fillIdealPerc = {54, 96, 95, 94};
+    int[] chlorinator_fillHighPerc = {55, 97, 96, 95};
     for (int i = 0; i < chlorinatorValve_names.length; i++) {
       devices.add(new FlexyDemoChlorinator(chlorinator_names[i], chlorinator_fillLowPerc[i],
           chlorinator_fillHighPerc[i], chlorinator_fillIdealPerc[i],
@@ -269,13 +239,10 @@ public class FlexyDemoScenarioWastewater extends FlexyDemoScenario {
     }
 
     // pH Sensors
-    String[] pHSensor_names =
-        {"PHSensor-A01", "PHSensor-A02", "PHSensor-A03", "PHSensor-A04", "PHSensor-B01",
-            "PHSensor-B02",
-            "PHSensor-B03", "PHSensor-B04"};
-    double[] pHSensor_pHLow = {7.0, 7.9, 7.9, 7.6, 8.0, 7.8, 7.8, 7.3};
-    double[] pHSensor_pHIdeal = {7.1, 8.0, 8.0, 7.7, 8.1, 7.9, 7.9, 7.4};
-    double[] pHSensor_pHHigh = {7.2, 8.1, 8.1, 7.8, 8.2, 8.0, 8.0, 7.5};
+    String[] pHSensor_names = {"PHSensor-A01", "PHSensor-A02", "PHSensor-B01", "PHSensor-B02"};
+    double[] pHSensor_pHLow = {7.0, 7.9, 8.0, 7.8};
+    double[] pHSensor_pHIdeal = {7.1, 8.0, 8.1, 7.9};
+    double[] pHSensor_pHHigh = {7.2, 8.1, 8.2, 8.0};
     for (int i = 0; i < pHSensor_names.length; i++) {
       devices.add(new FlexyDemoPHSensor(pHSensor_names[i], pHSensor_pHLow[i], pHSensor_pHHigh[i],
           pHSensor_pHIdeal[i]));
